38edf20b948a2f671ecdab0e09f9469005708456
zhang liang <zhangliang@linklogis.com>
2019-11-26 17:29:20 +0800

    update

--- tradingview-webhooks-bot/actions.py
+++ tradingview-webhooks-bot/actions.py
@@ -28,32 +28,9 @@ def calc_price(given_price):
     return price
 
 
-def send_order(data):
-
-    """
-    This function sends the order to the exchange using ccxt.
-    :param data: python dict, with keys as the API parameters.
-    :return: the response from the exchange.
-    """
-
-    # Replace kraken with your exchange of choice.
-    exchange = ccxt.kraken({
-        # Inset your API key and secrets for exchange in question.
-        'apiKey': '',
-        'secret': '',
-        'enableRateLimit': True,
-    })
-
-    # Send the order to the exchange, using the values from the tradingview alert.
-    print('Sending:', data['symbol'], data['type'], data['side'], data['amount'], calc_price(data['price']))
-    order = exchange.create_order(data['symbol'], data['type'], data['side'], data['amount'], calc_price(data['price']))
-    # This is the last step, the response from the exchange will tell us if it made it and what errors pop up if not.
-    print('Exchange Response:', order)
-
-
 def bitmex_order(data):
     client = bitmex.bitmex(test=False,
                            api_key="wBjcgxQfLBBTgSzaKtJCDh0E",
                            api_secret="CX543A3hE1oHj_O19u_nxouCCuwDK71VB3nDqMJxpPJzLBQC")
-    result = client.Instrument.Instrument_get(filter=json.dumps({'symbol': 'XBTJPY'})).result()
-    print("order res", result)
+    orderbook = client.OrderBook.OrderBook_getL2(symbol='XBTUSD', depth=20).result()
+    print("order res", orderbook)
